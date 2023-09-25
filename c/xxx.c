c5ee1edab93bafe08619e834b891cd5cd902c11f
test testing <saladbox207@protonmail.com>
2023-09-25 20:59:49 +0900

    add C practice dir

---
+++ c/xxx.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    printf("hello warudo! \n");
+    printf("nigerundayooo smokey \n");
+    printf("I like \n turtles! \n");
+    printf("meme desu \n");
+   return 0;
+}
