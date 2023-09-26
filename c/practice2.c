646b75b74f9a750c544e433fcee3e0c020082d27
test testing <saladbox207@protonmail.com>
2023-09-26 16:14:39 +0900

    add practice2 and fix practice2.c

--- c/practice2.c
+++ c/practice2.c
@@ -2,6 +2,40 @@
 
 int main (){
 
+// variable = Allocated space in memory to store a value.
+//            We can refer to a variable's NAME to access value stored in it.
+//            That variable will behave as if it's the value it contains.
+//            But we also need to declare what type of DATA we are storing (Integer, float, char, etc.)
 
-    
+// It starts with two crucial steps - Declaration and Initialization.
+
+int x; // declaration
+x = 1; // initialization
+
+// or u can do it at the same time
+
+int y = 5; // declaration + initialization
+
+// 'int' means Integer btw, it means.. whole numbers. NO DECIMALS ALLOWED
+// variables are allowed to be written as words.
+
+int age = 25;
+int bottles = 13;
+int eggs = 4;
+int boolets = 120;
+int elemental_energy = 100;
+int owned_characters = 24;
+
+// u got it, it can be written as words.
+// ------
+// 'float' means Floating Point Numbers (Fractional Numbers), 
+// or just think of it as numbers with decimals.
+
+float KPD = 2.5;
+float DKP = 510.5;
+float GDP = 5;
+float other_efficiency_stat = 1337.69;
+
+
+
 }
