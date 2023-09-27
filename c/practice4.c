248b511af6d0641bb2b773442e20d74f7fe95647
test testing <saladbox207@protonmail.com>
2023-09-27 23:23:13 +0900

    add p4

---
+++ c/practice4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>       // adds STanDart Input/Output.
+#include <stdbool.h>     // adds booleans. (u know, the TRUE and FALSE type)
+
+int main (){
+    
+    // Data types:                               // Description:                   Refer to it using:
+
+    char abby = 'X';                             // single character               %c
+    char r_book[] = "Long Variable Name";        // array of characters, words.    %s
+    double x, y, a;
+    
+
+   
+   // Print section:
+
+    printf("Simple char: %c",abby);
+    printf("\n");
+    printf("Some words: %s",r_book);
+
+    return 0;
+}
+    
