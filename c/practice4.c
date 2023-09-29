1114dfd3fa2110613ea27157209162c96b646747
test testing <saladbox207@protonmail.com>
2023-09-29 15:16:01 +0900

    mod p4

--- c/practice4.c
+++ c/practice4.c
@@ -3,19 +3,46 @@
 
 int main (){
     
-    // Data types:                               // Description:                   Refer to it using:
+    // %c = character
+    // %s = string (of characters) (words)
+    // %f = float (0.1)
+    // %lf = double (0.1)
+    // %d = integer
 
-    char abby = 'X';                             // single character               %c
-    char r_book[] = "Long Variable Name";        // array of characters, words.    %s
-    double x, y, a;
-    
+    // %.1 = decimal precision
+    // %1 = minimum field width
+    // %- = left align
+    // %
 
-   
-   // Print section:
+    float kolbasa = 243.55;
+    float doshirak = 62.00;
+    float tushonka = 53.99;
 
-    printf("Simple char: %c",abby);
+    // Print section 1:
+    // not redacted version
+    printf("\nPyatorochka shop:\n");
+    printf("Rolled meat costs %f per kg\n", kolbasa);
+    printf("Asian pasta costs %f per piece\n", doshirak);
+    printf("Canned cow meat costs %f per piece\n", tushonka);
     printf("\n");
-    printf("Some words: %s",r_book);
+
+    // Print section 2:
+    // redacted and randomly aligned version
+
+    printf("\nSupar Dupar Cool Shop:\n");
+    printf("Rolled meat costs $%40.2f per kg\n",kolbasa);             // %40.2 <- number after % refers to allocated space
+    printf("Asian pasta costs $%15.2f per piece\n", doshirak);
+    printf("Canned cow meat costs $%-20.2f per piece\n", tushonka);    // minus gives it LEFT alignment
+    printf("\n");
+
+    // Print section 3:
+    // perfectly aligned version
+
+    printf("\n --- Elite iFive shop ---\n");
+    printf("-> Rolled meat - %12.1f $ for 1kg\n",kolbasa);             
+    printf("-> Asian pasta - %11.1f $ for 1pc\n", doshirak);
+    printf("-> Canned cow meat - %7.1f $ for 1pc\n", tushonka);  
+    printf("\n");
 
     return 0;
 }
