4b97bc0908ca2b7570754b89adb523496616c539
test testing <saladbox207@protonmail.com>
2023-09-29 15:34:58 +0900

    add p6

---
+++ c/practice6_arithmetic_operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+    // arithmetic operators
+
+    //  +  (addition)
+    //  -  (subtraction)
+    //  *  (multiplication)
+    //  /  (division)
+    //  %  (modulus)
+    //  ++ (incriment)
+    //  -- (decrement)
+
+    int iks = 5;
+    int igrek = 3;
+
+
+    return(0);
+}
