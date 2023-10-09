2e279fc713ee632d5bddba2af10813f91639b4d4
test testing <saladbox207@protonmail.com>
2023-10-09 15:15:00 +0900

    add p10

---
+++ c/practice10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    // Section 1 - Declarization
+
+    int a, b, c, rezult;
+    
+    // Section 2 - Programm body
+
+    scanf("\n %d %d %d", &a, &b, &c);    // awaits for 3 user inputs
+    rezult = a + b + c;                  // then does whatever mathematics listed
+    printf("resultato: %d\n", rezult);   // and shows result.
+
+
+    return 0;
+}
