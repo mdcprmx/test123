d70087d46421d51393792e0f6ea76fc08d3c9f72
test testing <saladbox207@protonmail.com>
2023-10-14 14:40:28 +0900

    fix p13_math: syntax and commentary

--- c/practice13_math.c
+++ c/practice13_math.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-// programm to calculate distance between two points
+// programm used to calculate distance between two points
 // formula used: 
 //              AB = sqrt( pow((Xb -Xa), 2) + pow((Yb - Ya), 2) )
 
@@ -10,17 +10,21 @@ int main() {
 
     // Section 1 - declaration
 
-    double xo, yo, xt, yt, resx, resy, res;
+    double xo, yo, xt, yt;
+    double resx, resy, res;
+    // "xo" - is short for "x one". 
+    // "xt" - is short for "x two" 
+    // "yo" and "yt" is the same.
     scanf("%lf %lf %lf %lf", &xo, &yo, &xt, &yt);
 
     // Section 2 - body
 
-    resx = xt - xo;
+    resx = xt - xo;      // vnutri skobok
     resy = yt - yo;
-    resx = pow(resx, 2); // vozvedenie v kvadrad
+    resx = pow(resx, 2); // vozvedenie v kvadrat
     resy = pow(resy, 2);
     
-    res = resx + resy;
+    res = resx + resy;   // vnutri kornia
     res = sqrt(res);
 
     // Section 3 - printf
