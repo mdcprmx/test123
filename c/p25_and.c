8f1ba39bad44fb83371fa4abfad4b210c32448aa
test testing <saladbox207@protonmail.com>
2023-10-23 14:46:08 +0900

    add p25_and

---
+++ c/p25_and.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    // logical operators = && (AND). it checks for two conditions to be true.
+    //                     All conditions must be true, if even one condition
+    //                     is false then it will consider whole operation false. 
+
+    // 1 - declaration
+    bool wife = true;
+    bool kids = true;
+    bool home = true;
+    bool car = true;
+    
+
+    // 2 - main
+    if(wife && kids && home && car){        // if wife = true, kids = true, home = true, and car = then
+        printf("\nYou're a happy man!\n");  // then IF statement is true.
+    }
+    else{
+        printf("\nBro, ur sad I guess\n");
+    }
+
+    // 99 - end
+
+    ////////////////////////
+    ////////////////////////
+    ////////////////////////
+
+return 0;
+}
